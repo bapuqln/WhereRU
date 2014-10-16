df658182a4651ae7773b05e2558131ca8b92aa90
bapuqln <bapu_wang7@163.com>
2014-10-16 16:21:58 +0800

    Init
    Init WhereRU

---
+++ WhereRU/OtherInfoViewController.h
@@ -0,0 +1,15 @@
+//
+//  OtherInfoViewController.h
+//  WhereRU
+//
+//  Created by bapu on 10/16/14.
+//  Copyright (c) 2014 bapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OtherInfoViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>{
+
+}
+
+@end
---
+++ WhereRU/OtherInfoViewController.m
@@ -0,0 +1,62 @@
+//
+//  OtherInfoViewController.m
+//  WhereRU
+//
+//  Created by bapu on 10/16/14.
+//  Copyright (c) 2014 bapu. All rights reserved.
+//
+
+#import "OtherInfoViewController.h"
+
+@interface OtherInfoViewController ()
+
+@end
+
+@implementation OtherInfoViewController
+
+#pragma mark - 数据源方法
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 5;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString * identifier = @"otherUserInfoCell";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    
+    return cell;
+}
+
+#pragma mark - 代理方法
+#pragma mark 重新设置行高
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 10;
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
